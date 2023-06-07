deccf2e2e0a92e67672da0c1fecd44e420058aa3
Tanveer Iqbal <iqbal.tanveer@icloud.com>
2023-06-07 05:40:33 +0300

    initial commit

---
+++ app.config.ts
@@ -0,0 +1,41 @@
+export default defineAppConfig({
+  alpine: {
+    title: 'Etimad Arabian Technologies',
+    description: 'The minimalist blog theme',
+    image: {
+      src: '/social-card-preview.png',
+      alt: 'An image showcasing my project.',
+      width: 400,
+      height: 300
+    },
+    header: {
+      position: 'right', // possible value are : | 'left' | 'center' | 'right'
+      logo: {
+        path: '/logo.svg', // path of the logo
+        pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
+        alt: 'alpine' // alt of the logo
+      }
+    },
+    footer: {
+      credits: {
+        enabled: false, // possible value are : true | false
+        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+      },
+      navigation: true, // possible value are : true | false
+      alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
+      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+    },
+    socials: {
+      twitter: 'mockgee',
+      instagram: 'mockgee',
+      linkedin: {
+        icon: 'uil:linkedin',
+        label: 'LinkedIn',
+        href: 'https://www.linkedin.com/company/mockgeelabs'
+      }
+    },
+    form: {
+      successMessage: 'Message sent. Thank you!'
+    }
+  }
+})
